fb7fb9eb88e41582a2dd3ea2021a8fa1af0c9d50
Emil Babra <brittsboemil@gmail.com>
2019-12-15 22:15:27 +0100

    Add most of tables to database.

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,5 +1,8 @@
-DROP DATABASE IF EXISTS carRental;
-CREATE DATABASE carRental;
-USE carRental;
+drop database if exists carRental;
+create database carRental;
+use carRental;
 
-CREATE TABLE 
+create table cars (RegNr varchar(6) primary key, Year integer, Price integer);
+create table customers (SsNr integer(12) primary key, Name varchar(256), Adress varchar(256), [Postal adress] varchar(256));
+create table make (make varchar(256) primary key);
+create table color (color varchar(256) primary key);
